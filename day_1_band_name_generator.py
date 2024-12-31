08784439d7c65246c9b5a678d9307923bd2ff099
Jesal <jesaldave.dev@gmail.com>
2024-12-31 11:02:35 +0530

    Add files via upload

---
+++ day_1_band_name_generator.py
@@ -0,0 +1,7 @@
+print("What's the name of the city you grew up in ? \n")
+city_name = input()
+print("What's the name of your pet ?\n")
+pet_name = input()
+
+band_name = city_name + pet_name
+print("The name of your band could be :" + band_name )
