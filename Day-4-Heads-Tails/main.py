4771010f5aadf0e2a63daa80949cd6d083b9c351
Jesal <jesaldave.dev@gmail.com>
2024-12-31 12:17:19 +0530

    Create main.py

---
+++ Day-4-Heads-Tails/main.py
@@ -0,0 +1,16 @@
+#heads or tails random programs
+
+import random
+
+ #using random.randint function
+
+random_heads_tails = random.randint(0,1)
+print(random_heads_tails)
+
+#conditional logic
+
+if random_heads_tails == 0:
+    print("Heads")
+else:
+    print("Tails")
+        
